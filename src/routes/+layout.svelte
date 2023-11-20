00bc0b163039ee3ac43d7ce0aeefb1ce11fa22ea
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-19 21:19:15 -0700

    appshell for main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.postcss';
+	import {AppBar, AppShell} from '@skeletonlabs/skeleton';
+	import Navbar from '../lib/components/navBar/Navbar.svelte';
+	import Header from '../lib/components/header/Header.svelte';
+	import Footer from '../lib/components/footer/Footer.svelte';
+
 </script>
 
-<slot />
+
+
+	
+<AppShell>
+	<svelte:fragment slot="header"><Navbar></Navbar></svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter"><Footer></Footer></svelte:fragment>
+</AppShell>
+
