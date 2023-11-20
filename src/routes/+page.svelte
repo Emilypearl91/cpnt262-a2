1fe8a11332b5eb40aab05b5a13e39e847243ec10
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-19 21:26:28 -0700

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,13 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+import './about/+page.svelte';
+import './products/+page.svelte';
+import Header from '../lib/components/header/Header.svelte';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+</script>
+<Header></Header>
+<section class="m-4 text center ">
+  <h2 class="h2 text-center ">title</h2>
+  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non purus imperdiet velit dignissim consectetur quis vitae diam. Nunc feugiat ex a volutpat suscipit. Ut vulputate congue nisl ac gravida. Morbi vestibulum eget ligula vel placerat. Cras faucibus, lacus sed gravida sollicitudin, lorem nunc laoreet massa, et tempus ligula tellus malesuada massa. Vivamus efficitur enim scelerisque erat bibendum congue. Duis nunc enim, aliquet vitae laoreet sit amet, pellentesque quis nulla. Sed at est imperdiet, consequat tortor non, faucibus orci. Curabitur condimentum massa congue ligula blandit, semper dignissim dolor interdum. Suspendisse vel fringilla tortor. Duis quam ipsum, venenatis et hendrerit eget, maximus sit amet turpis. Nunc et nunc erat.
+  
+    Nulla faucibus, felis eu dignissim posuere, dui velit volutpat odio, quis lobortis ex libero in ante. Vestibulum viverra massa nec vulputate aliquam. Nulla porta, libero tincidunt porttitor facilisis, sapien neque consequat sem, eu efficitur enim augue at justo. Sed urna leo, sodales nec ex id, volutpat tempor ante. Fusce euismod est quis neque rutrum vehicula. Ut erat neque, porta eu condimentum non, sodales eget ligula. Nunc vel libero justo. In hac habitasse platea dictumst. Praesent aliquam leo arcu, sit amet maximus lectus sollicitudin eu. Praesent viverra turpis vitae aliquet maximus.</p>
+  </section>
