a71a4f6b6f4244b2d9d6fedd5316d80431430c16
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-19 21:26:42 -0700

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Header from "../../lib/components/header/Header.svelte";
+
+</script>
+
+<Header></Header>
+<section class="m-2">
+<h2 class="h2">title</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non purus imperdiet velit dignissim consectetur quis vitae diam. Nunc feugiat ex a volutpat suscipit. Ut vulputate congue nisl ac gravida. Morbi vestibulum eget ligula vel placerat. Cras faucibus, lacus sed gravida sollicitudin, lorem nunc laoreet massa, et tempus ligula tellus malesuada massa. Vivamus efficitur enim scelerisque erat bibendum congue. Duis nunc enim, aliquet vitae laoreet sit amet, pellentesque quis nulla. Sed at est imperdiet, consequat tortor non, faucibus orci. Curabitur condimentum massa congue ligula blandit, semper dignissim dolor interdum. Suspendisse vel fringilla tortor. Duis quam ipsum, venenatis et hendrerit eget, maximus sit amet turpis. Nunc et nunc erat.
+
+  Nulla faucibus, felis eu dignissim posuere, dui velit volutpat odio, quis lobortis ex libero in ante. Vestibulum viverra massa nec vulputate aliquam. Nulla porta, libero tincidunt porttitor facilisis, sapien neque consequat sem, eu efficitur enim augue at justo. Sed urna leo, sodales nec ex id, volutpat tempor ante. Fusce euismod est quis neque rutrum vehicula. Ut erat neque, porta eu condimentum non, sodales eget ligula. Nunc vel libero justo. In hac habitasse platea dictumst. Praesent aliquam leo arcu, sit amet maximus lectus sollicitudin eu. Praesent viverra turpis vitae aliquet maximus.</p>
+</section>
