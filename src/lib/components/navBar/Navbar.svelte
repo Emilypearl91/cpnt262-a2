683fb0e256cc47073a13925b7b54039fc7055df3
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-19 21:22:39 -0700

    custom components

---
+++ src/lib/components/navBar/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+import { AppBar } from '@skeletonlabs/skeleton';
+import { Avatar } from '@skeletonlabs/skeleton';
+
+import { title } from '../../data'; 
+import { initials } from '../../data';
+import { about } from '../../data';
+import { product } from '../../data';
+import { home } from '../../data';
+
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+  <svelte:fragment slot="lead">
+<!---profile icon--->
+    <Avatar initials="{initials}" background="bg-primary-500" />
+  </svelte:fragment>
+<!---shop name--->
+  <h1 class="h1">{title}</h1>
+  <svelte:fragment slot="trail">
+<!---navbar links--->
+<button type="button" class="btn variant-ghost-primary"><a href="{home.URL}">{home.title}</a></button>
+<button type="button" class="btn variant-ghost-primary"><a href="{product.URL}">{product.title}</a></button>
+<button type="button" class="btn variant-ghost-primary"><a href="{about.URL}">{about.title}</a></button>
+          </svelte:fragment>
+</AppBar>
+
+ 
---
+++ src/lib/data.js
@@ -0,0 +1,55 @@
+//three card components
+export let cornLettuce = {
+  title: 'Corn Lettuce Bowl',
+  description: 'This bowl is made with corn, lettuce, tomato and fish',
+  URL: 'src/lib/images/amy-syiek-4irgyik6Q3U-unsplash.jpg'
+};
+export let shrimpBowl = {
+  title: 'Shrimp Tomato Bowl',
+  description: 'This shrimp feta tomato bowl is made lettuce, tomato, croutons, parmesan, feta, pacific white shrimp and topped with our signature dressing made in house.',
+  URL: 'src/lib/images/ryan-concepcion-w_z0RJCSBiE-unsplash.jpg'
+};
+
+export let edamameSalmon = {
+  title: 'Edamame Salmon Bowl',
+  description: 'This edamame salmon bowl is made lettuce, corn, cucumber, tomato, cabbage, green onion, salmon and hard boiled eggs.',
+  URL: 'src/lib/images/anh-nguyen-kcA-c3f_3FE-unsplash.jpg' 
+};
+// bestseller card components 
+export let bestsellers = {
+  sesameTitle: 'Sesame Shrimp Bowl',
+  sesameInfo: 'This sesame shrimp bowl is made with seaweed, mushrooms, parsley, shrimp, rice noodles and tomatoes',
+  sesameURL: 'src/lib/images/food-photographer-FtpgFeUQuAY-unsplash.jpg',
+  radishTitle: 'Chicken Rice Bowl',
+  radishInfo: 'This chicken rice bowl is made with corn, rice, tomatoes and grilled chicken.',
+  radishURL: 'src/lib/images/sina-piryae-bBzjWthTqb8-unsplash.jpg',
+  chickenTitle: 'Chicken Rice Bowl',
+  chickenInfo: 'This chicken rice bowl is made with corn, rice, tomatoes and grilled chicken.',
+  chickenURL: 'src/lib/images/food-photographer-zhkhwGrqilw-unsplash.jpg'
+};
+
+//navbar
+export let title = 'Tossed';
+  export let initials = 'JD';
+
+  export let about = {
+    title: 'About Us',
+    URL: 'http://localhost:5173/about',
+  };
+ 
+  export let product = {
+    title: 'Products',
+    URL: 'http://localhost:5173/products'
+  };
+ export let home = {
+   title: 'Home',
+   URL: 'http://localhost:5173'
+ };
+
+// footer
+export let footer = {
+  facebook: 'https://fontawesome.com/icons/facebook?f=brands&s=solid',
+  instagram: 'https://fontawesome.com/icons/instagram?f=brands&s=solid',
+  location: 'https://fontawesome.com/icons/location-dot?f=classic&s=solid',
+  twitter: 'https://fontawesome.com/icons/twitter?f=brands&s=solid'
+};
