9a17a0076b80387bb93ece3348ba333b7457fbc9
Emily Pearl <Emilypearl9193@gmail.com>
2024-02-24 19:31:32 -0700

    fixed image rendering

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,3 +1,3 @@
 <section class="bg-surface-500 h-80">
-  <img class="w-screen h-80 bg-cover bg-top " src="src/lib/assets/header-image.jpg" alt="three salad bowls">
+  <img class="w-screen h-80 bg-cover bg-top " src="/images/header-image.jpg" alt="three salad bowls">
 </section>
--- src/lib/data.js
+++ src/lib/data.js
@@ -2,30 +2,30 @@
 export let cornLettuce = {
   title: 'Corn Lettuce Bowl',
   description: 'This bowl is made with corn, lettuce, tomato and fish',
-  URL: 'src/lib/assets/amy-syiek-4irgyik6Q3U-unsplash.jpg'
+  URL: '/images/amy-syiek-4irgyik6Q3U-unsplash.jpg'
 };
 export let shrimpBowl = {
   title: 'Shrimp Tomato Bowl',
   description: 'This shrimp feta tomato bowl is made lettuce, tomato, croutons, parmesan, feta, pacific white shrimp and topped with our signature dressing made in house.',
-  URL: 'src/lib/assets/ryan-concepcion-w_z0RJCSBiE-unsplash.jpg'
+  URL: '/images/ryan-concepcion-w_z0RJCSBiE-unsplash.jpg'
 };
 
 export let edamameSalmon = {
   title: 'Edamame Salmon Bowl',
   description: 'This edamame salmon bowl is made lettuce, corn, cucumber, tomato, cabbage, green onion, salmon and hard boiled eggs.',
-  URL: 'src/lib/assets/anh-nguyen-kcA-c3f_3FE-unsplash.jpg' 
+  URL: '/images/anh-nguyen-kcA-c3f_3FE-unsplash.jpg' 
 };
 // bestseller card components 
 export let bestsellers = {
   sesameTitle: 'Sesame Shrimp Bowl',
   sesameInfo: 'This sesame shrimp bowl is made with seaweed, mushrooms, parsley, shrimp, rice noodles and tomatoes',
-  sesameURL: 'src/lib/assets/food-photographer-FtpgFeUQuAY-unsplash.jpg',
+  sesameURL: '/images/food-photographer-FtpgFeUQuAY-unsplash.jpg',
   radishTitle: ' Radish Green Bowl',
   radishInfo: 'This radish green salad bowl is made with radishes, cucumber, onions, leafy green mix and pita bread.',
-  radishURL: 'src/lib/assets/sina-piryae-bBzjWthTqb8-unsplash.jpg',
+  radishURL: '/images/sina-piryae-bBzjWthTqb8-unsplash.jpg',
   chickenTitle: 'Chicken Rice Bowl',
   chickenInfo: 'This chicken rice bowl is made with corn, rice, tomatoes and grilled chicken.',
-  chickenURL: 'src/lib/assets/food-photographer-zhkhwGrqilw-unsplash.jpg'
+  chickenURL: '/images/food-photographer-zhkhwGrqilw-unsplash.jpg'
 };
 
 //navbar
