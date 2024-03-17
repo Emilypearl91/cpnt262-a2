c352d93cde138c848110d97999d321960bb509ae
Emily Pearl <Emilypearl9193@gmail.com>
2024-03-17 17:22:40 -0600

    moved products from data file and card component file to product page

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,36 +1,48 @@
-//three card components
-export let cornLettuce = {
-  title: 'Corn Lettuce Bowl',
-  description: 'This bowl is made with corn, lettuce, tomato and fish',
-  URL: '/images/amy-syiek-4irgyik6Q3U-unsplash.jpg'
-};
-export let shrimpBowl = {
+// today's special cards
+export let products = [
+  {id: 1,
+    title: 'Corn Lettuce Bowl',
+    description: 'This bowl is made with corn, lettuce, tomato and fish',
+    URL: '/images/amy-syiek-4irgyik6Q3U-unsplash.jpg',
+    price: 12,
+  quantity: 1},
+  {
+    id: 2,
   title: 'Shrimp Tomato Bowl',
   description: 'This shrimp feta tomato bowl is made lettuce, tomato, croutons, parmesan, feta, pacific white shrimp and topped with our signature dressing made in house.',
-  URL: '/images/ryan-concepcion-w_z0RJCSBiE-unsplash.jpg'
-};
-
-export let edamameSalmon = {
-  title: 'Edamame Salmon Bowl',
-  description: 'This edamame salmon bowl is made lettuce, corn, cucumber, tomato, cabbage, green onion, salmon and hard boiled eggs.',
-  URL: '/images/anh-nguyen-kcA-c3f_3FE-unsplash.jpg' 
-};
-// bestseller card components 
-export let bestsellers = {
-  sesameTitle: 'Sesame Shrimp Bowl',
-  sesameInfo: 'This sesame shrimp bowl is made with seaweed, mushrooms, parsley, shrimp, rice noodles and tomatoes',
-  sesameURL: '/images/food-photographer-FtpgFeUQuAY-unsplash.jpg',
-  radishTitle: ' Radish Green Bowl',
-  radishInfo: 'This radish green salad bowl is made with radishes, cucumber, onions, leafy green mix and pita bread.',
-  radishURL: '/images/sina-piryae-bBzjWthTqb8-unsplash.jpg',
-  chickenTitle: 'Chicken Rice Bowl',
-  chickenInfo: 'This chicken rice bowl is made with corn, rice, tomatoes and grilled chicken.',
-  chickenURL: '/images/food-photographer-zhkhwGrqilw-unsplash.jpg'
-};
+  URL: '/images/ryan-concepcion-w_z0RJCSBiE-unsplash.jpg',
+  price: 12,
+  quantity: 1
+  },
+  {id: 3,
+    title: 'Edamame Salmon Bowl',
+    description: 'This edamame salmon bowl is made lettuce, corn, cucumber, tomato, cabbage, green onion, salmon and hard boiled eggs.',
+    URL: '/images/anh-nguyen-kcA-c3f_3FE-unsplash.jpg',
+    price: 12,
+  quantity: 1},
+    {id: 4,
+      title: 'Sesame Shrimp Bowl',
+      info: 'This sesame shrimp bowl is made with seaweed, mushrooms, parsley, shrimp, rice noodles and tomatoes',
+      URL: '/images/food-photographer-FtpgFeUQuAY-unsplash.jpg',
+      price: 12,
+    quantity: 1},
+    { id: 5,
+      title: ' Radish Green Bowl',
+      info: 'This radish green salad bowl is made with radishes, cucumber, onions, leafy green mix and pita bread.',
+      URL: '/images/sina-piryae-bBzjWthTqb8-unsplash.jpg',
+      price: 12,
+    quantity: 1},
+    {id: 6,
+      title: 'Chicken Rice Bowl',
+      info: 'This chicken rice bowl is made with corn, rice, tomatoes and grilled chicken.',
+      URL: '/images/food-photographer-zhkhwGrqilw-unsplash.jpg',
+      price: 12,
+    quantity: 1}
+];
 
 //navbar
 export let title = 'Tossed';
-  export let initials = 'JD';
+  
 
   export let about = {
     title: 'About Us',
